dda04e565b25b4a95232633db9015a83361d11df
SukuSnk <136146956+SukuSnk@users.noreply.github.com>
2023-06-10 13:32:23 +0530

    Update config.py
    Signed-off-by: SukuSnk <136146956+SukuSnk@users.noreply.github.com>

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/a75dc6811d7b8b918091c.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Dramaticclubbb")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dramatic_clubb")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
